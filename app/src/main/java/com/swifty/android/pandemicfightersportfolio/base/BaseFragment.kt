0c62e586525ebd69df0c923cc7568ed32439e0d4
kasia <kasia.kowalczyk@swifty.global>
2024-01-14 16:42:50 +0100

    init commit

---
+++ app/src/main/java/com/swifty/android/pandemicfightersportfolio/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.swifty.android.pandemicfightersportfolio.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflateViewBinding(inflater, container)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    abstract fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): T
+}
---
+++ app/src/main/java/com/swifty/android/pandemicfightersportfolio/home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.swifty.android.pandemicfightersportfolio.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+}
