f327cab35f0fc638af580a13b969a3c12c459e68
kasia <kasia.kowalczyk@swifty.global>
2024-01-15 17:53:53 +0100

    loading map

--- app/src/main/java/com/android/pandemic/fighters/home/HomeFragment.kt
+++ app/src/main/java/com/android/pandemic/fighters/home/HomeFragment.kt
@@ -1,14 +1,36 @@
 package com.android.pandemic.fighters.home
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.android.pandemic.fighters.base.BaseFragment
 import com.android.pandemic.fighters.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(), OnMapReadyCallback {
 
     override fun inflateViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding.mapView.apply {
+            onCreate(savedInstanceState)
+            getMapAsync(this@HomeFragment)
+        }
+        return binding.root
+    }
+
+
+    override fun onMapReady(map: GoogleMap) {
+        //todo adding pins
+    }
 }
